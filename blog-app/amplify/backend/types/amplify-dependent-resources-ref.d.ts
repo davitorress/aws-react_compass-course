78f2cb384d4483bac3431b4d56f7e262149759e1
davitorress <dev.davitorres@gmail.com>
2023-02-28 14:49:05 -0300

    feat: amplify and api setup

---
+++ blog-app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "blogapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  }
+}
